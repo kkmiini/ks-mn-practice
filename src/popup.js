668d6dacf573fc240e2a06547687119ba686488b
kkmiini <vuy100@naver.com>
2025-02-16 13:56:24 +0900

    html, css

--- src/popup.js
+++
@@ -1,8 +0,0 @@
-function showPopup(message) {
-  document.getElementById("popup-message").innerText = message;
-  document.getElementById("popup").style.display = "flex";
-}
-
-function closePopup() {
-  document.getElementById("popup").style.display = "none";
-}
