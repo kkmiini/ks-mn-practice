b4132828ecd9c91640e2def1ef9a6aaeaf6bcb8f
kkmiini <vuy100@naver.com>
2025-02-19 18:18:36 +0900

    game play, popup page

--- pointgame/pointgame/src/main/resources/static/js/popup.js
+++ pointgame/pointgame/src/main/resources/static/js/popup.js
@@ -1,8 +1,33 @@
-function showPopup(message) {
-  document.getElementById("popup-message").innerText = message;
-  document.getElementById("popup").style.display = "flex";
-}
+    document.addEventListener("DOMContentLoaded", function() {
+        // 모든 결과 값을 가져옴
+        let results = document.querySelectorAll(".result-content");
+        let getpoint = 0;
 
-function closePopup() {
-  document.getElementById("popup").style.display = "none";
-}
+
+        // 결과 중 "あたり"가 있는지 확인
+        results.forEach(function(result) {
+            if (result.innerText.trim() === "あたり") {
+				
+				if(results.length <= 5) {
+					getpoint = 1000;
+				
+					} else if(results.length <= 7) {
+						getpoint = 500;
+						
+						} else {
+							getpoint = 200;
+							
+						}
+				alert("ゲーム成功!"  + "取得ポイント" + getpoint );
+            } 	
+            
+            
+            if (result.innerText.trim() !== "あたり" && results.length === 10) {
+				getpoint = 0;
+        		alert("ゲーム失敗。" + "取得ポイント" + getpoint) ;
+    		}
+        });
+    });
+    
+    
+   
